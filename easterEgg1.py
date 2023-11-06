63ac3f9699de8f741a894cf3dad6918b51fd0e6e
dameun2224 <113423804+dameun2224@users.noreply.github.com>
2023-11-06 23:28:06 +0900

    Update easterEgg1.py

--- easterEgg1.py
+++ easterEgg1.py
@@ -1,46 +1,24 @@
 # 이스터에그 함수 1
-# 이스터에그 조건에 맞아 출력이 있는 경우 return 0
-# 이스터에그 조건에 맞지 않아 출력이 없는 경우 return 1
-def easterEgg1(arr):
-    flag = 1
 
-    # 모든 입력이 '*'인 경우 "STAR!" 출력
-    if all(x == '*' for x in arr):
-        print("STAR!")
-        flag = 0
+def easterEgg1(num):
+    # 모든 입력이 '*'인 경우 "HELP!" 출력
+    if num == 7534:
+        print("=> HELP!")
 
-    # 하나의 입력이 들어왔고, 입력이 "777"인 경우 "JACKPOT!" 출력
-    elif len(arr) == 1 and arr[0] == "777":
-        print("JACKPOT!")
-        flag = 0
+    # 입력이 "777"인 경우 "JACKPOT!" 출력
+    elif num == 777:
+        print("=> JACKPOT!")
 
-    # 하나의 입력이 들어왔고, 입력이 "404"인 경우 "404 NOT FOUND" 출력
-    elif len(arr) == 1 and arr[0] == "404":
-        print("404 NOT FOUND")
-        flag = 0
+    # 입력이 "404"인 경우 "404 NOT FOUND" 출력
+    elif num == 404:
+        print("=> 404 NOT FOUND")
 
-    # 하나의 입력이 들어왔고, 입력이 "100"인 경우 "Perfect score!" 출력
-    elif len(arr) == 1 and arr[0] == "100":
-        print("Perfect score!")
-        flag = 0
+    # 입력이 "100"인 경우 "Perfect score!" 출력
+    elif num == 100:
+        print("=> Perfect score!")
 
-    if(flag == 1) : return 1
-    else : return 0
+    # 입력이 "0"인 경우 "ZERO" 출력
+    elif num == 0 :
+        print("=> ZERO")
 
-# 계산 함수
-def calculator(arr):
-    # 계산 함수 구현
-    result = 0
-
-    # **마지막 결과 출력 부분에 추가해주세요**
-    # 결과가 0인 경우 0이 아닌 "ZERO" 출력
-    if(result == 0):
-        print("ZERO")
-    else:
-        print(result)
-
-    return 0;
-
-def main():
-
-    return 0;
+    return 0
