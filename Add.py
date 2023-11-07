2c5f1fb0559bfb5e66928aeb9d5186e95a128771
youth5427 <127531421+youth5427@users.noreply.github.com>
2023-11-07 14:26:45 +0900

    Update Add.py

--- Add.py
+++ Add.py
@@ -1,13 +1,11 @@
 def Add(input):
+    # unit test를 위한 주석
+    #input = [13, '+', 2, '+', 4, '+', 2]
+    
+    result = input[0]    # 초기 결과값을 0번지로 초기화
+    for i in range(1, len(input)): # 0번지로 이미 초기화 했기 때문에 1부터 시작(뺄셈 때문에)
+        if input[i] != '+':
+            result = result + int(input[i])
 
-    #input = '  3+2+4+2 '
+    print(result)
 
-    # 앞뒤 공백 삭제 후 연산자를 공백으로 교체
-    modified_str = input.strip().replace("+"," ")
-
-    # 문자열을 공백 기준으로 분리하여 정수로 변환
-    numbers = list(map(int, modified_str.split()))
-
-    # 숫자들의 합 계산
-    result = sum(numbers)
-    print(result)
