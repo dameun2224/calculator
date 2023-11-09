7502b16f5712074def8017d730d47a74fd0a2704
dameun2224 <113423804+dameun2224@users.noreply.github.com>
2023-11-09 17:57:56 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,24 @@
-#2023.11.8(수) calculator Version.0
-#연산자 함수 Add, Sub, Mul
+#2023.11.9(목) calculator Version.2
 
+#연산자 함수 Add, Sub, Mul
 def calculate(inputNum):
     def Add(inputNum):
         sum=inputNum[0]
-        for i in range (1, len(inputNum)):
-            if i % 2 == 0:
-                sum += inputNum[i]
+        for i in range (2, len(inputNum),2):
+            sum += inputNum[i]
         return sum
-
 
     def Sub(inputNum):
         sum = inputNum[0]
-        for i in range(1, len(inputNum)): 
-            if i % 2 == 0:
-                sum -= inputNum[i]
+        for i in range(2, len(inputNum),2): 
+            sum -= inputNum[i]
         return sum
 
     def Mul(inputNum):
         sum = inputNum[0]
-        for i in range (1, len(inputNum)):
-            if i % 2 == 0:
-                sum *= inputNum[i]
+        for i in range (2, len(inputNum),2):
+            sum *= inputNum[i]
         return sum
-
 
     if inputNum[1]=='+':
         result = Add(inputNum)
@@ -39,9 +34,7 @@ def calculate(inputNum):
     inputNum.clear()
             
             
-
 # 이스터에그 함수
-
 def easterEgg(inputNum):
 
     easter1 = [63, '-', 430, '-', 3428]
@@ -59,7 +52,7 @@ def easterEgg(inputNum):
     }
 
     if inputNum[-1] in easter_dict:
-        print(easter_dict[int(inputNum[-1])])
+        print(easter_dict[inputNum[-1]])
 
     for i in range(len(inputNum) - len(easter1) + 1):
         if inputNum[i:i+len(easter1)] == easter1:
@@ -82,101 +75,85 @@ def easterEgg(inputNum):
             print("앞뒤가 똑같은 대리운전 (1577)")
             print("앞뒤가 똑같은 번호 1577")
             print("대리운전 1577")
-            
 
-
-# error 함수
-
-def error(inputNum):
-    # 1. 리스트의 첫 번째 원소부터 홀수 번째에 정수가 입력되었는지 확인_정환(main)
-    # 2. 리스트의 짝수 번째에 +, -, * 연산자가 입력되었는지 확인_정환(main)
-    
-    # 3. 입력된 연산자가 한 가지 종류의 연산자만 입력되었는지 확인
-    operators = set(inputNum[1::2])
-    operators.discard(inputNum[-1])
-
-    if len(operators) != 1:
-        print("ERROR: 한 가지 종류의 연산자만 입력하세요")
-        print("--------------")
-        return 0
-
-    # 4. 입력된 수가 임계값을 넘었는지 확인
-    for i in range(0, len(inputNum), 2):
-        if inputNum[i] >= 100000:
-            print("ERROR: 입력된 수가 임계값 100000을 넘었습니다.")
-            print("--------------")
-            return 0
-    return 1
-
-
-# 음수를 숫자로 판별하는 메서드
-
-def is_negative_numeric(input_string): 
-    try: 
-        int(input_string) # 문자열을 정수로 변환하려 시도 
-        return True # 변환이 성공하면 True를 반환 
-    except ValueError: 
-        return False # 변환이 실패하면 False를 반환
-
-
+def printList(inputLine):
+    for e in inputLine:
+        print(e)
 
 # main 함수
-
 def main():
-    inputNum = []
+    inputLine = []
     print("계산기 시작 (프로그램을 종료하려면 exit를 입력하세요)")
     while True:
         line = input()
 
-        # 입력이 연산자인경우
-        if line in ['+', '-', '*','=']:
-            # 마지막 입력이 연산자 혹은 첫 입력이 연산자인 경우
-            if len(inputNum) == 0 or inputNum[-1] in ['+', '-', '*']:
+        # 루프 종료 조건
+        if line == "exit" : break
+        
+        # 입력이 연산자인 경우
+        elif line in ['+', '-', '*','=']:
+            # 첫번째 입력이 연산자인 경우 & 연산자가 연속 2번 입력된 경우
+            if len(inputLine) == 0 or inputLine[-1] in ['+', '-', '*']:
                 print("ERROR: 연산자가 아닌 숫자를 입력하세요")
                 print("--------------")
-                inputNum.clear()
+                printList(inputLine)
                 continue
-            else: # 그 전 입력이 숫자인 경우
-                inputNum.append(line) # 연산자 추가
-                
-                # 여기서 부턴 = 에 대한 연산 진행
-                if line == '=':
-                    # 에러 함수에 값 전달
-                    # if 에러가 없으면: 연산자 함수에 값 전달 후 아래 내용 진행
-                    if error(inputNum) == 1:
-                        calculate(inputNum)
-                    inputNum.clear()
+            # 입력이 '='인 경우
+            elif line in '=':
+                # 숫자가 하나만 있는 경우 (ex, 3 = 이 들어오면 3 출력)
+                if len(inputLine) == 1:
+                    print(inputLine[0])
+                    print("--------------")
+                    inputLine.clear()
+                    continue
+                # 결과 출력 
+                else:
+                    calculate(inputLine)
+                    inputLine.clear()
+                    continue
+            # 입력된 연산자가 처음 입력된 연산자와 다른 경우
+            elif len(inputLine) > 2 and line != inputLine[1]:
+                print("ERROR: 한 가지 종류의 연산자만 입력하세요")
+                print("--------------")
+                printList(inputLine)
+                continue
+            # 위 경우에 걸리지 않는 경우 - 정상
+            else:
+                inputLine.append(line)
+                continue
+        
+        # 입력이 숫자 혹은 그 외의 경우
+        else:
+            # 정수 입력의 경우
+            try:
+                num = int(line)
+                if len(inputLine) == 0:
+                    inputLine.append(num)
+                    easterEgg(inputLine)
+                    continue
+                # 숫자가 연속 2번 들어온 경우
+                elif type(inputLine[-1]) is int:
+                    print("ERROR: 잘못된 값이 입력되었습니다. 연산자를 입력하세요")
+                    print("--------------")
+                    printList(inputLine)
+                    continue
+                # 숫자가 임계값을 넘어간 경우
+                elif num > 100000:
+                    print("ERROR: 입력된 수가 임계값 100000을 넘었습니다.")
+                    print("--------------")
+                    printList(inputLine)
+                    continue
+                # 첫번째 입력이 숫자인 경우 & 이전 입력이 연산자인 경우 -> 정상
+                else: 
+                    inputLine.append(num)
+                    easterEgg(inputLine)
                     continue
 
-        elif line == 'exit':
-            break
-
-        else: # 현재 입력이 숫자일 경우
-            if is_negative_numeric(line):
-                inputNum.append(int(line))
-                
-                easterEgg(inputNum)
+            # 잘못된 입력의 경우 (연산자 또는 정수가 아님)
+            except ValueError:
+                print("ERROR: 잘못된 값이 입력되었습니다. 정수 또는 연산자(+,-,*)를 입력하세요")
+                print("--------------")
+                printList(inputLine)
                 continue
-            else:
-                try:
-                    int(line) #문자열을 정수로 변환하려 시도 - 실패시 except
-                    # 전 입력 값이 숫자가 아닌 경우
-                    if len(inputNum) !=0 and not type(inputNum[-1]) is int:
-                        inputNum.append(line)
-                        continue
-                    elif len(inputNum) == 0:
-                        inputNum.append(line)
-                        continue
-                    else:
-                        print("ERROR: 잘못된 값이 입력되었습니다. 숫자를 입력하세요")
-                        print("--------------")
-                        inputNum.clear()
-                        continue
 
-                except ValueError: # 연산자 아닌데 숫자도, =도 아니거나 그 전값이 숫자인 경우
-                        print("ERROR: 잘못된 값이 입력되었습니다. 정수 또는 연산자(+,-,*)를 입력하세요")
-                        print("--------------")
-                        inputNum.clear()
-                        continue
-            
 main()
